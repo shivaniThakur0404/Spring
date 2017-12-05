3ed5e5603a8187f2008756b5b110eba854ae542e
shivaniThakur0404 <shivanithakur0404@gmail.com>
2017-12-05 13:59:57 +0530

    CRUD API

---
+++ CrudSpring/src/main/java/com/demo/CrudSpring/dao/EmployeeDao.java
@@ -0,0 +1,20 @@
+package com.demo.CrudSpring.dao;
+import java.util.List;
+
+import com.demo.CrudSpring.model.Employee;
+
+public interface EmployeeDao {
+	
+	List<Employee> getEmpDetails();
+	
+	//List<UserDetails> getUser(int id);
+	
+	Employee getEmployeeById(int id );
+	 
+	String deleteEmployee(int id); 
+	 
+	String updateEmp(Employee employee);
+	
+	String createEmployee(Employee employee);
+
+}
---
+++ CrudSpring/src/main/java/com/demo/CrudSpring/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.demo.CrudSpring.service;
+
+import java.util.List;
+
+import com.demo.CrudSpring.model.Employee;
+
+
+
+public interface EmployeeService {
+
+	List<Employee> getEmpDetails();	
+	
+	Employee getEmployeeById(int id );
+		 
+	String deleteEmployee(int id); 
+		 
+	String updateEmp(Employee employee);
+		
+	String createEmployee(Employee employee);
+}
